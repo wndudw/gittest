9623ab5cd78d2ffce7f819c094012f5934186f5e
JOO <wndudw@gmail.com>
2022-04-06 15:26:39 +0900

    새로운 branch 생성!

---
+++ src/newbranch/BranchClass.java
@@ -0,0 +1,14 @@
+package newbranch;
+
+public class BranchClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		System.out.println("새로운 branch 생성");
+		
+	
+		
+	}
+}
