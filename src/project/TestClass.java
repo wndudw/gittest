15cd5562c3591002ac17a0a34f13b8c1e0cba7bd
JOO <wndudw@gmail.com>
2022-04-06 14:17:17 +0900

    두번째 커밋

--- src/project/TestClass.java
+++ src/project/TestClass.java
@@ -4,7 +4,12 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
+		
 		System.out.println("testing");
+		
+		String str = "new";
+		
 	}
 
 }
